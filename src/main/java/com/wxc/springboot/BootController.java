b13cca0ec53d25c8be0e40d5b2c22b0651bc2a0b
wuxuecheng2010 <240480126@qq.com>
2018-02-11 09:36:19 +0800

    test2

--- src/main/java/com/wxc/springboot/BootController.java
+++ src/main/java/com/wxc/springboot/BootController.java
@@ -4,11 +4,15 @@ package com.wxc.springboot;
 import com.wxc.entity.User;
 import com.wxc.entity.properties.CarProperties;
 import com.wxc.springboot.entity.BookStore;
+import com.wxc.springboot.entity.Gril;
 import com.wxc.springboot.service.BookStoreService;
+import com.wxc.springboot.service.GrilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -79,5 +83,48 @@ public class BootController {
 
     }
 
+
+    @PostMapping("/saveBookStore")
+    public BookStore saveBookStore(@RequestParam(value = "storeName") String storeName,
+                                   @RequestParam(value = "address") String address) {
+        BookStore bookStore = new BookStore();
+        bookStore.setAddress(storeName);
+        bookStore.setStoreName(address);
+        return bookStoreService.saveBookStore(bookStore);
+    }
+
+
+    @PostMapping("/saveBookStore2")
+    public BookStore saveBookStore2(BookStore bookStore) {
+        BookStore bookStore2 = new BookStore();
+        bookStore2.setAddress(bookStore.getAddress());
+        bookStore2.setStoreName(bookStore.getStoreName());
+        return bookStoreService.saveBookStore(bookStore);
+    }
+
+    @Autowired
+    private GrilService grilService;
+
+    //加字段验证
+    @PostMapping("/saveGril")
+    public Object saveGril(@Valid Gril gril, BindingResult result) {
+
+        if (result.hasErrors()) {
+            System.out.println(result.getFieldError().getDefaultMessage());
+            return result.getFieldError();
+        }
+        return grilService.saveGril(gril);
+
+    }
+
+
+    //统一异常处理
+        // 定义自己的业务逻辑异常处理类ExceptionHandler，  类名和方法名称任意，注意的是注释
+        // result 为自己定义的一个表示结果的类  任意
+    @PostMapping("/getGrilAge/{id}")
+    public Gril getGril(@PathVariable(value = "id") Integer id)throws Exception{
+
+     return    grilService.getGril(id);
+    }
 
 }
